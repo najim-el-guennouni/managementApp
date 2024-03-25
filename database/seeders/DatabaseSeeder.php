d68eb48ca1f42c018f5a94b2a5e33384f6bdd13e
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2024-03-25 23:43:11 +0000

    prepare project and database

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'najim',
+            'email' => 'najimelguennouni@gmail.com',
+            'password' => bcrypt('123456789'),
+            'email_verified_at' => time()
+        ]);
+        Project::factory()->count(30)
+                          ->hasTasks(30)
+                          ->create();
+    }
+}
