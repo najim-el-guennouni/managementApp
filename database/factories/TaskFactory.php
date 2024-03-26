864ae6029045d0c57655ea89aa7c8dad66076336
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2024-03-26 13:42:56 +0000

    create Controller and Define Routes

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -26,6 +26,8 @@ class TaskFactory extends Factory
             'assigned_user_id' => 1,
             'created_by' => 1,
             'updated_by' => 1,
+            'created_at' => time(),
+            'updated_at' => time(),
 
         ];
     }
