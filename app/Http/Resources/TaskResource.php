a037bb1d5203d72c3106f6cf82dc2c43305e6933
najim el guennouni <60970889+najim-el-guennouni@users.noreply.github.com>
2024-03-28 23:44:53 +0000

    Sorting of Projects and Render Tasks

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TaskResource extends JsonResource
@@ -14,6 +15,19 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
-    }
+        return [
+            'id' =>$this->id,
+            'name'=>$this->name,
+            'description'=> $this->description,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d'),
+            'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
+            'status' => $this->status,
+            'priority' => $this->status,
+            'image_path' => $this->image_path,
+            'project' => new ProjectResource($this->project),
+            'assignedUser' =>$this->assignedUser ?
+             new UserResource($this->assignedUser) : null,
+            'createdBy' => new UserResource($this->createdBy),
+            'updatedBy' => new UserResource($this->updatedBy),
+        ];    }
 }
